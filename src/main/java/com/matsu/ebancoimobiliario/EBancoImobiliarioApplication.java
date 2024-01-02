60e70969bec66c28259ce6ba14ff60e8dc13776e
Stefanie Silva <matsustefanie@gmail.com>
2024-01-02 10:42:24 -0300

    Project start

---
+++ src/main/java/com/matsu/ebancoimobiliario/EBancoImobiliarioApplication.java
@@ -0,0 +1,13 @@
+package com.matsu.ebancoimobiliario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBancoImobiliarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBancoImobiliarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matsu/ebancoimobiliario/EBancoImobiliarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matsu.ebancoimobiliario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBancoImobiliarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
